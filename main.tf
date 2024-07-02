af484082863d91fa82b1b215e83e1fb40d38efe7
s1-slappey <s1.slappey@gmail.com>
2024-07-01 19:23:46 -0500

    Adding binaries and updating code. Still not 100%

--- main.tf
+++ main.tf
@@ -1,11 +1,26 @@
 provider "aws" {
-  region = "us-east-1"  # Change the region if needed
+  region = "us-east-2"  # Change the region if needed
 }
 
-resource "aws_key_pair" "deployer_key" {
-  key_name   = "deployer_key"
-  public_key = file("~/.ssh/id_rsa.pub")  # Path to your public SSH key
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
 }
+
+#resource "aws_key_pair" "deployer_key" {
+#  key_name   = "ec2-key-pair"
+#  public_key = file("~/.ssh/id_rsa.pub")  # Path to your public SSH key
+#}
 
 resource "aws_security_group" "allow_ssh" {
   name_prefix = "allow_ssh"
@@ -21,8 +36,8 @@ resource "aws_security_group" "allow_ssh" {
 
   ingress {
     description = "HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -40,22 +55,30 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  ami           = data.aws_ami.amazon_linux.id # Amazon Linux 2 AMI
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer_key.key_name
+  key_name   = "ec2-key-pair"
   security_groups = [aws_security_group.allow_ssh.name]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y java-1.8.0-openjdk
-              sudo amazon-linux-extras install java-openjdk11
-              sudo yum install -y git
-              git clone https://github.com/example/log4j-vulnerable-app.git /home/ec2-user/log4j-vulnerable-app
-              cd /home/ec2-user/log4j-vulnerable-app
-              ./gradlew build
-              ./gradlew run
-              EOF
+user_data = <<-EOF
+            #!/bin/bash
+            sudo -i
+            yum update -y
+            amazon-linux-extras install epel
+            yum install java-1.8.0-openjdk
+            amazon-linux-extras install java-openjdk11
+            yum install -y git
+            wget -c http://services.gradle.org/distributions/gradle-8.8-all.zip -P /tmp
+            unzip /tmp/gradle-8.8-all.zip -d /opt
+            ln -s /opt/gradle-8.8 /opt/gradle
+            printf "export GRADLE_HOME=/opt/gradle\nexport PATH=\$PATH:\$GRADLE_HOME/bin\n" > /etc/profile.d/gradle.sh
+            source /etc/profile.d/gradle.sh
+            git clone https://github.com/s1-slappey/cnapp_demo.git /home/ec2-user/app
+            cd /home/ec2-user/app/log4j-app
+            gradle build
+            gradle run
+            EOF
+
 
   tags = {
     Name = "Log4jVulnerableInstance"
